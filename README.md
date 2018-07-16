5add12687748fcc56a0deea6f5c15d0bb671540c
hihebark <hihebark@gmail.com>
2018-07-16 23:52:39 +0100

    added draw line to squarebox ...

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ gore
 <p align="center"><img src="Cover1.jpg" width="800"></p>
 
 <p align="center"><img src="GoreProject.png" width="800"></p>
+
+Steps
+-----
+
+- [x] Grayscal the image.
+- [ ] Compare every pixel to the pixels that surrounding it <- difficult getting black image. or use the other method split the image into 16*16 images.
+- [ ] Find face.
+- [ ] Output the image surrounding the face(s) with rectangle box.
+- [ ] Analyze the face.
+- [ ] Compare.
+- [ ] Predection.
+
--- core/squarebox.go
+++ core/squarebox.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 )
 
@@ -9,20 +10,36 @@ type squarebox struct {
 	x0, y0, x1, y1 int
 }
 
-func drawSquareBox (s squarebox, im draw.Image) error{
+func drawSquareBox(s squarebox, im draw.Image) error {
 	draw.Draw(im,
 		image.Rectangle{
 			Min: image.Point{
-				X:s.x0,
-				Y:s.y0,
+				X: s.x0,
+				Y: s.y0,
 			},
 			Max: image.Point{
-				X:s.x1,
-				Y:s.y1,
+				X: s.x1,
+				Y: s.y1,
 			},
 		},
 		image.Transparent,
 		image.ZP,
 		draw.Src)
 	return nil
+}
+
+func DrawLine(start, end image.Point, img image.Image, thick int, c color.Color) image.Image {
+	newimg := image.NewRGBA(img.Bounds())
+	for x := 0; x >= newimg.Bounds().Max.X; x++ {
+		for y := 0; y >= newimg.Bounds().Max.Y; y++ {
+			pix := img.At(x, y)
+			switch {
+			case y == start.Y || x == end.X && x <= start.X || x >= end.X:
+				newimg.Set(x, y, c)
+			default:
+				newimg.Set(x, y, pix)
+			}
+		}
+	}
+	return newimg
 }
