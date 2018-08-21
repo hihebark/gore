b119ed8f8829eef45103093d7734859cf6fdc17c
hihebark <n.amara@protonmail.ch>
2018-08-21 22:21:37 +0100

    add flag options

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/hihebark/gore/core"
 )
@@ -9,9 +10,17 @@ import (
  For more check the link --v
  https://medium.com/@ageitgey/machine-learning-is-fun-part-4-modern-face-recognition-with-deep-learning-c3cffc121d78
 *******************************************************************************/
+var img *string
+
+func init() {
+	img = flag.String("img", "", "Image")
+}
 
 func main() {
+	fmt.Printf(" ┏ ┳ ┓\n ┣ o ┫\n ┗ ┻ ┛\n")
 	fmt.Println(" Gore - 0.0.0")
-	core.Start("data/harold.png")
-	//core.Start("data/RMS.png")
+	if *img != "" {
+		core.Start("data/harold.png")
+		//core.Start("data/RMS.png")
+	}
 }
