7b3a048fb71c10cb722fd3716984c4870e0359ac
hihebark <n.amara@protonmail.ch>
2018-08-22 15:24:46 +0100

    poopush

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ import (
 var img *string
 
 func init() {
-	img = flag.String("img", "", "Image")
+	img = flag.String("img", "", "Path to the image.")
 }
 
 func main() {
 	fmt.Printf(" ┏ ┳ ┓\n ┣ o ┫\n ┗ ┻ ┛\n")
 	fmt.Println(" Gore - 0.0.0")
+	flag.Parse()
 	if *img != "" {
-		core.Start("data/harold.png")
-		//core.Start("data/RMS.png")
+		core.Start(*img)
 	}
 }
