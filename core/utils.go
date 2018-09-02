0b5c6962f6ced08ab93e2dd558283641a9f5c1d1
hihebark <n.amara@protonmail.ch>
2018-09-03 00:00:19 +0100

    poopush

--- core/utils.go
+++ core/utils.go
@@ -16,24 +16,7 @@ func GradientVector(x, y float64) (float64, float64) {
 	return magnitude, orientation
 }
 
-/*
 //DrawLine draw a line in image.
-func DrawLine(p image.Point, angle, length float64, imgsrc image.Image, c color.Color) *image.RGBA {
-	bounds := imgsrc.Bounds()
-	mask, dstimg := image.NewRGBA(bounds), image.NewRGBA(bounds)
-	x2 := math.Round(float64(p.X) + (length * math.Cos(angle)))
-	y2 := math.Round(float64(p.Y) + (length * math.Sin(angle)))
-	x2m := math.Round(float64(p.X) + (length * math.Cos(angle+180)))
-	slop := (x2 - float64(p.X)) / (y2 - float64(p.Y))
-	b := int(float64(p.Y) - slop*float64(p.X))
-	for x := int(x2m); x <= int(x2); x++ {
-		mask.Set(x, int(slop*float64(x))+b, c)
-	}
-	draw.Draw(dstimg, imgsrc.Bounds(), imgsrc, bounds.Min, draw.Src)
-	draw.Draw(dstimg, mask.Bounds(), mask, bounds.Min, draw.Over)
-	return dstimg
-}*/
-//func drawline(p image.Point, ori, len float64, c color.Color, imgsrc image.Image) *image.RGBA {
 func DrawLine(p image.Point, angle, length float64, imgsrc image.Image, c color.Color) *image.RGBA {
 	bound := imgsrc.Bounds()
 	dstimg, mask := image.NewRGBA(bound), image.NewRGBA(bound)
