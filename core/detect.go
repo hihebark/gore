ea0079a642a2d4a39cdfbfcd549aeb4a81362dc8
hihebark <n.amara@protonmail.ch>
2018-09-05 00:14:15 +0100

    poopush

--- core/detect.go
+++ core/detect.go
@@ -1,12 +1,13 @@
 package core
 
 import (
-	"color"
 	"fmt"
 	"image"
+	//"image/color"
 )
 
 //DetectFace
 func DetectFace(imgsrc, imgmodel image.Image) image.Rectangle {
+	fmt.Printf("+ Searching for face(s)...\n")
 	return image.Rect(0, 0, 0, 0)
 }
