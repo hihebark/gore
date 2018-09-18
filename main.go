e6c4f634734b8a1c3dba6ff4ad586091d6b9f38f
hihebark <n.amara@protonmail.ch>
2018-09-19 00:41:11 +0100

    changing main

--- main.go
+++ main.go
@@ -16,61 +16,54 @@ const maxsizex int = 600
 
 var (
 	path  *string
-	scale *int
+	model *string
 )
 
 func init() {
 	path = flag.String("p", "", "Path to the image.")
-	scale = flag.Int("s", 2, "Scale image into the given s.")
+	model = flag.String("model", "", "Model to use hog, sal, ...")
+
 }
 
 func main() {
 	fmt.Printf("  ┏ ┳ ┓\n  ┣ o ┫\n  ┗ ┻ ┛\n")
 	fmt.Println("  Gore - 0.0.1")
 	flag.Parse()
-	if *path == "" {
+	switch {
+	case *path != "" && *model != "":
+		img, err := os.Open(*path)
+		defer img.Close()
+		if err != nil {
+			fmt.Printf("image:os.Open path:%s\n", *path)
+		}
+
+		info, _ := img.Stat()
+		name := strings.Split(info.Name(), ".")[0]
+		imgdec, format, err := image.Decode(img)
+		if err != nil {
+			fmt.Printf("error while decoding image: %v\n", err)
+			panic("Decode")
+		}
+		i := core.NewImageInfo(format, name, imgdec.Bounds(), 2, 17)
+		if imgdec.Bounds().Max.X > maxsizex {
+			imgdec = i.Scale(imgdec)
+		}
+		gray := i.Grayscale(imgdec)
+		i.Save("gray", gray)
+		break
+	case *path == "":
+		rgb := core.RGBAtoRGB(color.RGBA{255, 0, 0, 128})
+		xyz := core.RGBtoXYZ(rgb)
+		lab := core.XYZtoCieLAB(xyz)
+		fmt.Printf("rgb: %v\nxyz: %v\nlab:%v\n", rgb, xyz, lab)
+	default:
 		flag.PrintDefaults()
 		os.Exit(2)
 	}
-	img, err := os.Open(*path)
-	defer img.Close()
-	if err != nil {
-		fmt.Printf("image:os.Open path:%s\n", *path)
-	}
-
-	info, _ := img.Stat()
-	name := strings.Split(info.Name(), ".")[0]
-	imgdec, format, err := image.Decode(img)
-	if err != nil {
-		fmt.Printf("error while decoding image: %v\n", err)
-		panic("Decode")
-	}
-	i := core.NewImageInfo(format, name, imgdec.Bounds(), *scale, 17)
-	if imgdec.Bounds().Max.X > maxsizex {
-		imgdec = i.Scale(imgdec)
-	}
-	gray := i.Grayscale(imgdec)
-	i.Save("gray", gray)
-	rgb := core.RGBAtoRGB(color.RGBA{255, 0, 0, 128})
-	xyz := core.RGBtoXYZ(rgb)
-	lab := core.XYZtoCieLAB(xyz)
-	fmt.Printf("rgb: %v\nxyz: %v\nlab:%v\n", rgb, xyz, lab)
 	//i.Save("blur", model.Salience(imgdec, 3, 1))
 	/*
 		gray := i.Grayscale(imgdec)
 		imghog := model.HogVect(gray, i)
 		i.Save("hog", imghog)
-		if err != nil {
-			fmt.Printf("image:os.Open path:%s\n", path)
-		}
-		imgmodel, _, _ := image.Decode(imgm)
-		points := core.DetectFace(imghog, imgmodel)
-		fmt.Printf("Points: %v\n", points)
-		c := color.RGBA{255, 255, 0, 255}
-		for _, p := range points {
-			imgdec = core.DrawSquare(imgdec, p.Rect, 1, c)
-		+= imgsrc.At(kx, ky).RGBA()
-		i.Save("Square", imgdec)
-		i.Wg.Wait()
 	*/
 }
